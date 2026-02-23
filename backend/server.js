8680bc1acbf16486c0eb740178817cad376f5c62
manjeet98 <manjeet9871124@gmail.com>
2026-02-23 18:55:59 +0530

    manjeet commit

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://unique-truffle-fa8a4f.netlify.app/",
     credentials: true
 }));
 
