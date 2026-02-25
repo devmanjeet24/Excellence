bff835229239d7777ccfdde097d925932bd28d24
manjeet98 <manjeet9871124@gmail.com>
2026-02-25 19:41:32 +0530

    fcghchgv

--- backend/server.js
+++ backend/server.js
@@ -17,16 +17,28 @@ const PORT = process.env.PORT || 5000;
 
 
 const corsOptions = {
-  origin: "https://spontaneous-chebakia-c0484d.netlify.app",
-//   origin: "http://localhost:5173",
- methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
+        origin: [
+            "http://localhost:5173",
+            "https://spontaneous-chebakia-c0484d.netlify.app",
+            "https://pretelegraph-bella-summarily.ngrok-free.dev",
+        ], methods: ['GET', 'POST', 'PUT', 'DELETE'], credentials: true
+    };
+
+
+    
 
 
 app.use(cors(corsOptions));
-// app.options("*", (req, res) => res.sendStatus(200));
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(express.json());
 app.use(cookieParser());
--- frontend/src/api/Axios.js
+++ frontend/src/api/Axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: "https://excellence-z1kz.onrender.com/api",
+  // baseURL: "https://excellence-z1kz.onrender.com/api",
+  baseURL: "https://pretelegraph-bella-summarily.ngrok-free.dev/api",
   withCredentials: true,
 });
 
