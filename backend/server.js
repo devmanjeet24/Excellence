a94aa8d9674768d7c4dc4756bc56a76630ec7497
manjeet98 <manjeet9871124@gmail.com>
2026-02-26 10:06:14 +0530

    manjeet commit 26

--- backend/server.js
+++ backend/server.js
@@ -19,26 +19,22 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
         origin: [
             "http://localhost:5173",
-            "https://spontaneous-chebakia-c0484d.netlify.app",
-            "https://pretelegraph-bella-summarily.ngrok-free.dev",
+            // "https://spontaneous-chebakia-c0484d.netlify.app",
         ], methods: ['GET', 'POST', 'PUT', 'DELETE'], credentials: true
     };
 
 
-    
-
-
 app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    return res.sendStatus(200);
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     return res.sendStatus(200);
+//   }
+//   next();
+// });
 
 app.use(express.json());
 app.use(cookieParser());
--- frontend/src/api/Axios.js
+++ frontend/src/api/Axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "http://localhost:5000/api",
+  baseURL: "http://localhost:5000/api",
   // baseURL: "https://excellence-z1kz.onrender.com/api",
-  baseURL: "https://pretelegraph-bella-summarily.ngrok-free.dev/api",
   withCredentials: true,
 });
 
