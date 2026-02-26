e701ba5b2ea5d930125a50c8ba6cc39dc24ca60c
devmanjeet24 <96927480+devmanjeet24@users.noreply.github.com>
2026-02-26 10:17:04 +0530

    Change API base URL to production endpoint

--- frontend/src/api/Axios.js
+++ frontend/src/api/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://excellence-t0d1.onrender.com/api",
   // baseURL: "https://excellence-z1kz.onrender.com/api",
   withCredentials: true,
 });
